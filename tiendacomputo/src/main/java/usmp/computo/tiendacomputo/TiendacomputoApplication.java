272a1d7263671d23bfa574cf854a1753fe8ca1cb
lorenperalta <stinpc@hotmail.com>
2021-04-15 22:56:28 -0500

    maven initial structure

---
+++ tiendacomputo/src/main/java/usmp/computo/tiendacomputo/TiendacomputoApplication.java
@@ -0,0 +1,13 @@
+package usmp.computo.tiendacomputo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendacomputoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendacomputoApplication.class, args);
+	}
+
+}
---
+++ tiendacomputo/src/test/java/usmp/computo/tiendacomputo/TiendacomputoApplicationTests.java
@@ -0,0 +1,13 @@
+package usmp.computo.tiendacomputo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendacomputoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
